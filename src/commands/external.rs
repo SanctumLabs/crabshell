4f4d99101e1370c4c38b52f69b23e77a43e76809
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-11-07 16:26:02 +0300

    fix(type): traverse directory tree to locate executable files

--- src/commands/external.rs
+++ src/commands/external.rs
@@ -1,5 +1,6 @@
 use super::Command::{self, *};
 use std::{io, io::Write, process::Command as StdCommand};
+use std::os::unix::fs::PermissionsExt;
 
 pub(crate) fn external_cmd(cmd: &Command) {
     let External { args, path, .. } = cmd else {
@@ -18,16 +19,37 @@ pub(crate) fn parse_external_cmd(cmd: &str, args: &str) -> Option<Command> {
     let Ok(path_env) = std::env::var("PATH") else {
         return None;
     };
-    path_env
-        .split(':')
-        .map(|path| {
-            let full_path = format!("{}/{}", path, cmd);
-            std::fs::metadata(&full_path).map(|_| full_path)
-        })
-        .find_map(Result::ok)
-        .map(|path| External {
-            cmd: cmd.to_string(),
-            args: args.to_string(),
-            path,
-        })
+
+    for dir in path_env.split(':') {
+        let full_path = format!("{}/{}", dir, cmd);
+        let path = std::path::Path::new(&full_path);
+        if path.exists() {
+            if let Ok(metadata) = path.metadata() {
+                if metadata.permissions().mode() & 0o111 != 0 {
+                    if let Ok(canonical_path) = path.canonicalize() {
+                        return Some(External {
+                            cmd: cmd.to_string(),
+                            args: args.to_string(),
+                            path: canonical_path.display().to_string(),
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    None
+
+    // path_env
+    //     .split(':')
+    //     .map(|path| {
+    //         let full_path = format!("{}/{}", path, cmd);
+    //         std::fs::metadata(&full_path).map(|_| full_path)
+    //     })
+    //     .find_map(Result::ok)
+    //     .map(|path| External {
+    //         cmd: cmd.to_string(),
+    //         args: args.to_string(),
+    //         path,
+    //     })
 }
